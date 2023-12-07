f99fd973dcf4cd6d9ff0bd4630fe43e3d400af37
AaKim <aarony12@naver.com>
2023-12-07 20:29:06 +0900

    231207

--- Assets/RoadDrawer/Editor/LaneDrawerEditor.cs
+++ Assets/RoadDrawer/Editor/LaneDrawerEditor.cs
@@ -1,13 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-// using log4net.Util;
 
-// using Barmetler.RoadSystem;
-// using NUnit.Framework;
-// using PlasticPipe.PlasticProtocol.Messages;
-// using TMPro;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -19,7 +13,6 @@ using UnityEngine;
 [CanEditMultipleObjects]
 public class LaneDrawerEditor : Editor
 {
-    // public MyRoad road { get; private set; }
 
     SerializedProperty item1;
     SerializedProperty item2;
@@ -45,6 +38,8 @@ public class LaneDrawerEditor : Editor
         item3 = serializedObject.FindProperty("Others");
         item4 = serializedObject.FindProperty("Segments");
         item5 = serializedObject.FindProperty("segment_scale");
+
+        
         
 
     }
